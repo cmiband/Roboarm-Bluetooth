d1f31eff06f5a97bacd89502db7d7d362887d3ab
Bartosz Adamowicz <33941168+cmiband@users.noreply.github.com>
2026-04-05 22:48:27 +0200

    added basic endpoints

--- BluetoothConnection/main.cpp
+++ BluetoothConnection/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include "httplib.h"
 
-int main() {
-	std::cout << "test" << std::endl;
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+
+int main() {	
+	httplib::Server server;
+
+    server.Get("/handshake", [](const httplib::Request&, httplib::Response& res) {
+        res.status = 200;
+    });
+
+    server.WebSocket("/ws", [](const httplib::Request& req, httplib::ws::WebSocket& ws) {
+        std::string msg;
+        while (ws.read(msg)) {
+            std::cout << msg << std::endl;
+        }
+    });
+
+	server.listen("0.0.0.0", 8080);
+
 	return 0;
 }
